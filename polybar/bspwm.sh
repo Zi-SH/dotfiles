1882d5e4baf85769a53b537196fb7daa7cc9a1b3
Ｚｉｐｓ「 ☕ 」 <37426682+Zi-SH@users.noreply.github.com>
2020-04-11 14:17:03 -0400

    Create bspwm.sh

---
+++ polybar/bspwm.sh
@@ -0,0 +1,12 @@
+# Render polybar bars
+
+# Center monitor
+polybar left --config=~/.config/polybar/bspwm/center &
+polybar center --config=~/.config/polybar/bspwm/center &
+polybar right --config=~/.config/polybar/bspwm/center &
+
+# Left monitor
+polybar left --config=~/.config/polybar/bspwm/left &
+
+# Right monitor
+polybar left --config=~.config/polybar/bspwm/right &
